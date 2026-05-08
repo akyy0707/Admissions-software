56ae0ceb44d93c3f3894bf24c90d04894b925327
akyy0707 <kyy07072005@gmail.com>
2026-05-08 15:10:01 +0700

    fix

--- src/main/java/com/tuyensinh/main/Main.java
+++ src/main/java/com/tuyensinh/main/Main.java
@@ -1,10 +1,13 @@
 package com.tuyensinh.main;
 
+import javax.security.auth.login.LoginContext;
+
 import com.tuyensinh.GUI.ImportExcelFrm;
+import com.tuyensinh.GUI.LoginForm;
 
 public class Main {
   public static void main(String[] args) {
-    new ImportExcelFrm().setVisible(true);
+    new LoginForm().setVisible(true);
   }
 }
 
